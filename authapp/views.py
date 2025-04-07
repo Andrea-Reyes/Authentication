633fa9d36969da6b06a992fac574faeec6893bc1
Raquel-Reyes <karen.reyeslem@gmail.com>
2025-04-07 16:57:11 -0600

    Actualización de views.py

--- authapp/views.py
+++ authapp/views.py
@@ -1,34 +1,88 @@
-from django.shortcuts import render
-from .models import User
-from django.db import models
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from .encriptacion import validatePassword, cryptPassword
+from django.contrib import messages
+from .forms import LoginForm, RegisterForm
+from .encriptacion import cryptPassword
+from .models import User
+from django.http import HttpResponse
+from django.contrib.auth import logout
 
 def login_view(request):
+    form = LoginForm()
+
     if request.method == "POST":
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        
-        try:
-            user = User.objects.get(email=email)
-            if validatePassword(password, user.password):
-                return JsonResponse({"message": "Login exitoso"})
-            else:
-                return JsonResponse({"error": "Contraseña incorrecta"}, status=400)
-        except User.DoesNotExist:
-            return JsonResponse({"error": "Usuario no encontrado"}, status=404)
-    
-    return render(request, "login.html")
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            try:
+                user = User.objects.get(email=email) 
+                if user.password == cryptPassword(password):  
+                    #messages.success(request, "¡Bienvenido!")
+
+                    #Crear la sesión 'cookie' y agregar los datos
+                    request.session['user_id'] = user.id
+                    request.session['username'] = email
+                    
+                    #Agregar la durabilidad
+                    request.session.set_expiry[3600]
+
+                    #return redirect('dashboard')
+                    return render(request,'dashboard.html')
+                else:
+                    #messages.error(request, "Datos incorrectos")
+                    return render(request,'error.html')
+            except User.DoesNotExist:
+                messages.error(request, "Datos incorrectos")
+
+    return render(request, 'login.html', {'form': form})
 
 def register_view(request):
+    form = RegisterForm()
+
     if request.method == "POST":
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        role = request.POST.get("role", "user")
-        
-        encrypted_password = cryptPassword(password)
-        User.objects.create(email=email, password=encrypted_password, role=role)
-        return JsonResponse({"message": "Registro exitoso"})
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            role = form.cleaned_data.get("role", "user")
+
+            if User.objects.filter(email=email).exists():
+                messages.error(request, "El usuario ya está registrado")
+                return render(request, 'register.html', {'form': form})
+
+            encrypted_password = cryptPassword(password)
+            User.objects.create(email=email, password=encrypted_password, role=role)
+
+            return redirect('login')
+
+    return render(request, 'register.html', {'form': form})
+
+def dashboard_view(request):
+    user_id = request.user_id
+    usuario = request.usuario
+    return HttpResponse(f'Bienvenido {usuario}, su ID es {user_id}')
+
+    #Lo hicimos en clase
+    #if 'user_id' in request.session:
+    #    user_id = request.session["user_id"]
+    #    usuario = request.session["username"]
+
+    #    return HttpResponse(f'Bienvenido {usuario}, su ID es {user_id}')
+    #else:
+    #    return redirect('login')
     
-    return render(request, "register.html")
+    #Lo que tenía de primero
+    #if request.user.is_authenticated:
+    #    return render(request, 'dashboard.html')
+    #else:
+    #    return redirect('login')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def error_view(request):
+    return render(request, 'error.html')
