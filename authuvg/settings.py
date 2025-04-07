48dbd970292c49b8eef319c0c1edb0a3489f945e
Raquel-Reyes <karen.reyeslem@gmail.com>
2025-04-07 16:59:51 -0600

    Actualización de settings.py

--- authuvg/settings.py
+++ authuvg/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'authapp.middlewares.authMiddleware.AuthMiddleware'
 ]
 
 ROOT_URLCONF = 'authuvg.urls'
